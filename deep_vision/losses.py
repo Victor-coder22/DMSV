d3ceb6794eeca68ffecc15f9bb7a8f796b7c9fbc
Victor <victor2212.ho@gmail.com>
2025-09-19 17:00:40 +0200

    Delete deep_vision directory

--- deep_vision/losses.py
+++
@@ -1,5 +0,0 @@
-from tensorflow import keras
-from functools import partial
-
-def no_background_sparse_categorical_crossentropy():
-    return partial(keras.losses.sparse_categorical_crossentropy, ignore_class=255)
